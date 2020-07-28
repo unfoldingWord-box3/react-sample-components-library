dfcf4a5ac019182c7bb36310a370949bc45f2805
mannycolon <colonmanuel7@gmail.com>
2020-07-28 10:31:37 -0400

    Init

---
+++ src/components/Button/Button.md
@@ -0,0 +1,9 @@
+# Button
+
+A very Basic button.
+
+```jsx
+import Button from "./Button";
+
+<Button text="Hello RCL devs!" />
+```
---
+++ src/components/MuiButton/MuiButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const MuiButton = ({ text }) => (
+  <Button variant="contained" color="primary">
+    {text}
+  </Button>
+);
+
+export default MuiButton;
